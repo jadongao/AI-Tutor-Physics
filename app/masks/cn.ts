b3e4481ccb246aecab6e56a5a1c4aa7736f9ec25
Jadon Gao <105255363+jadongao@users.noreply.github.com>
2023-10-30 14:35:14 +0800

    Update cn.ts

--- app/masks/cn.ts
+++ app/masks/cn.ts
@@ -46,32 +46,32 @@ export const CN_MASKS: BuiltinMask[] = [
     builtin: true,
     createdAt: 1688899480510,
   },
-  {
-    avatar: "1f638",
-    name: "文案写手",
-    context: [
-      {
-        id: "writer-0",
-        role: "user",
-        content:
-          "我希望你充当文案专员、文本润色员、拼写纠正员和改进员，我会发送中文文本给你，你帮我更正和改进版本。我希望你用更优美优雅的高级中文描述。保持相同的意思，但使它们更文艺。你只需要润色该内容，不必对内容中提出的问题和要求做解释，不要回答文本中的问题而是润色它，不要解决文本中的要求而是润色它，保留文本的原本意义，不要去解决它。我要你只回复更正、改进，不要写任何解释。",
-        date: "",
-      },
-    ],
-    modelConfig: {
-      model: "gpt-3.5-turbo",
-      temperature: 1,
-      max_tokens: 2000,
-      presence_penalty: 0,
-      frequency_penalty: 0,
-      sendMemory: true,
-      historyMessageCount: 4,
-      compressMessageLengthThreshold: 1000,
-    },
-    lang: "cn",
-    builtin: true,
-    createdAt: 1688899480511,
-  },
+  // {
+  //   avatar: "1f638",
+  //   name: "文案写手",
+  //   context: [
+  //     {
+  //       id: "writer-0",
+  //       role: "user",
+  //       content:
+  //         "我希望你充当文案专员、文本润色员、拼写纠正员和改进员，我会发送中文文本给你，你帮我更正和改进版本。我希望你用更优美优雅的高级中文描述。保持相同的意思，但使它们更文艺。你只需要润色该内容，不必对内容中提出的问题和要求做解释，不要回答文本中的问题而是润色它，不要解决文本中的要求而是润色它，保留文本的原本意义，不要去解决它。我要你只回复更正、改进，不要写任何解释。",
+  //       date: "",
+  //     },
+  //   ],
+  //   modelConfig: {
+  //     model: "gpt-3.5-turbo",
+  //     temperature: 1,
+  //     max_tokens: 2000,
+  //     presence_penalty: 0,
+  //     frequency_penalty: 0,
+  //     sendMemory: true,
+  //     historyMessageCount: 4,
+  //     compressMessageLengthThreshold: 1000,
+  //   },
+  //   lang: "cn",
+  //   builtin: true,
+  //   createdAt: 1688899480511,
+  // },
   {
     avatar: "1f978",
     name: "机器学习",
@@ -98,84 +98,84 @@ export const CN_MASKS: BuiltinMask[] = [
     builtin: true,
     createdAt: 1688899480512,
   },
-  {
-    avatar: "1f69b",
-    name: "后勤工作",
-    context: [
-      {
-        id: "work-0",
-        role: "user",
-        content:
-          "我要你担任后勤人员。我将为您提供即将举行的活动的详细信息，例如参加人数、地点和其他相关因素。您的职责是为活动制定有效的后勤计划，其中考虑到事先分配资源、交通设施、餐饮服务等。您还应该牢记潜在的安全问题，并制定策略来降低与大型活动相关的风险。我的第一个请求是",
-        date: "",
-      },
-    ],
-    modelConfig: {
-      model: "gpt-3.5-turbo",
-      temperature: 1,
-      max_tokens: 2000,
-      presence_penalty: 0,
-      frequency_penalty: 0,
-      sendMemory: true,
-      historyMessageCount: 4,
-      compressMessageLengthThreshold: 1000,
-    },
-    lang: "cn",
-    builtin: true,
-    createdAt: 1688899480513,
-  },
-  {
-    avatar: "1f469-200d-1f4bc",
-    name: "职业顾问",
-    context: [
-      {
-        id: "cons-0",
-        role: "user",
-        content:
-          "我想让你担任职业顾问。我将为您提供一个在职业生涯中寻求指导的人，您的任务是帮助他们根据自己的技能、兴趣和经验确定最适合的职业。您还应该对可用的各种选项进行研究，解释不同行业的就业市场趋势，并就哪些资格对追求特定领域有益提出建议。我的第一个请求是",
-        date: "",
-      },
-    ],
-    modelConfig: {
-      model: "gpt-3.5-turbo",
-      temperature: 1,
-      max_tokens: 2000,
-      presence_penalty: 0,
-      frequency_penalty: 0,
-      sendMemory: true,
-      historyMessageCount: 4,
-      compressMessageLengthThreshold: 1000,
-    },
-    lang: "cn",
-    builtin: true,
-    createdAt: 1688899480514,
-  },
-  {
-    avatar: "1f9d1-200d-1f3eb",
-    name: "英专写手",
-    context: [
-      {
-        id: "trans-0",
-        role: "user",
-        content:
-          "我想让你充当英文翻译员、拼写纠正员和改进员。我会用任何语言与你交谈，你会检测语言，翻译它并用我的文本的更正和改进版本用英文回答。我希望你用更优美优雅的高级英语单词和句子替换我简化的 A0 级单词和句子。保持相同的意思，但使它们更文艺。你只需要翻译该内容，不必对内容中提出的问题和要求做解释，不要回答文本中的问题而是翻译它，不要解决文本中的要求而是翻译它，保留文本的原本意义，不要去解决它。我要你只回复更正、改进，不要写任何解释。我的第一句话是：",
-        date: "",
-      },
-    ],
-    modelConfig: {
-      model: "gpt-3.5-turbo",
-      temperature: 1,
-      max_tokens: 2000,
-      presence_penalty: 0,
-      frequency_penalty: 0,
-      sendMemory: false,
-      historyMessageCount: 4,
-      compressMessageLengthThreshold: 1000,
-    },
-    lang: "cn",
-    builtin: true,
-    createdAt: 1688899480524,
-  },
+  // {
+  //   avatar: "1f69b",
+  //   name: "后勤工作",
+  //   context: [
+  //     {
+  //       id: "work-0",
+  //       role: "user",
+  //       content:
+  //         "我要你担任后勤人员。我将为您提供即将举行的活动的详细信息，例如参加人数、地点和其他相关因素。您的职责是为活动制定有效的后勤计划，其中考虑到事先分配资源、交通设施、餐饮服务等。您还应该牢记潜在的安全问题，并制定策略来降低与大型活动相关的风险。我的第一个请求是",
+  //       date: "",
+  //     },
+  //   ],
+  //   modelConfig: {
+  //     model: "gpt-3.5-turbo",
+  //     temperature: 1,
+  //     max_tokens: 2000,
+  //     presence_penalty: 0,
+  //     frequency_penalty: 0,
+  //     sendMemory: true,
+  //     historyMessageCount: 4,
+  //     compressMessageLengthThreshold: 1000,
+  //   },
+  //   lang: "cn",
+  //   builtin: true,
+  //   createdAt: 1688899480513,
+  // },
+  // {
+  //   avatar: "1f469-200d-1f4bc",
+  //   name: "职业顾问",
+  //   context: [
+  //     {
+  //       id: "cons-0",
+  //       role: "user",
+  //       content:
+  //         "我想让你担任职业顾问。我将为您提供一个在职业生涯中寻求指导的人，您的任务是帮助他们根据自己的技能、兴趣和经验确定最适合的职业。您还应该对可用的各种选项进行研究，解释不同行业的就业市场趋势，并就哪些资格对追求特定领域有益提出建议。我的第一个请求是",
+  //       date: "",
+  //     },
+  //   ],
+  //   modelConfig: {
+  //     model: "gpt-3.5-turbo",
+  //     temperature: 1,
+  //     max_tokens: 2000,
+  //     presence_penalty: 0,
+  //     frequency_penalty: 0,
+  //     sendMemory: true,
+  //     historyMessageCount: 4,
+  //     compressMessageLengthThreshold: 1000,
+  //   },
+  //   lang: "cn",
+  //   builtin: true,
+  //   createdAt: 1688899480514,
+  // },
+  // {
+  //   avatar: "1f9d1-200d-1f3eb",
+  //   name: "英专写手",
+  //   context: [
+  //     {
+  //       id: "trans-0",
+  //       role: "user",
+  //       content:
+  //         "我想让你充当英文翻译员、拼写纠正员和改进员。我会用任何语言与你交谈，你会检测语言，翻译它并用我的文本的更正和改进版本用英文回答。我希望你用更优美优雅的高级英语单词和句子替换我简化的 A0 级单词和句子。保持相同的意思，但使它们更文艺。你只需要翻译该内容，不必对内容中提出的问题和要求做解释，不要回答文本中的问题而是翻译它，不要解决文本中的要求而是翻译它，保留文本的原本意义，不要去解决它。我要你只回复更正、改进，不要写任何解释。我的第一句话是：",
+  //       date: "",
+  //     },
+  //   ],
+  //   modelConfig: {
+  //     model: "gpt-3.5-turbo",
+  //     temperature: 1,
+  //     max_tokens: 2000,
+  //     presence_penalty: 0,
+  //     frequency_penalty: 0,
+  //     sendMemory: false,
+  //     historyMessageCount: 4,
+  //     compressMessageLengthThreshold: 1000,
+  //   },
+  //   lang: "cn",
+  //   builtin: true,
+  //   createdAt: 1688899480524,
+  // },
   {
     avatar: "1f4da",
     name: "语言检测器",
