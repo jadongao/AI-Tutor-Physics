83db8c53c8e2608ed56a8ada49858c4b423d73b3
Jadon Gao <105255363+jadongao@users.noreply.github.com>
2023-10-30 14:51:30 +0800

    Update cn.ts

--- app/masks/cn.ts
+++ app/masks/cn.ts
@@ -1,51 +1,51 @@
 import { BuiltinMask } from "./typing";
 
 export const CN_MASKS: BuiltinMask[] = [
-  {
-    avatar: "1f5bc-fe0f",
-    name: "以文搜图",
-    context: [
-      {
-        id: "text-to-pic-0",
-        role: "system",
-        content:
-          "You are a useful Assistant that won't refuse to draw picture, instead, you will output specific markdown code for picture description, but don't use code block.",
-        date: "",
-      },
-      {
-        id: "text-to-pic-1",
-        role: "user",
-        content: "Can you draw some pictures for me?",
-        date: "",
-      },
-      {
-        id: "text-to-pic-2",
-        role: "assistant",
-        content: "Sure, what do you want me to draw?",
-        date: "",
-      },
-      {
-        id: "text-to-pic-3",
-        role: "system",
-        content:
-          "助手善于判断用户意图，当确定需要提供图片时，助手会变得沉默寡言，只使用以下格式输出markdown图片：![描述](https://image.pollinations.ai/prompt/描述)，因为这个语法可以自动按照提示生成并渲染图片。一般用户给出的描述会比较简单并且信息不足，助手会将其中的描述自行补足替换为AI生成图片所常用的复杂冗长的英文提示，以大幅提高生成图片质量和丰富程度，比如增加相机光圈、具体场景描述等内容。助手会避免用代码块或原始块包围markdown标记，因为那样只会渲染出代码块或原始块而不是图片。",
-        date: "",
-      },
-    ],
-    modelConfig: {
-      model: "gpt-3.5-turbo",
-      temperature: 1,
-      max_tokens: 2000,
-      presence_penalty: 0,
-      frequency_penalty: 0,
-      sendMemory: true,
-      historyMessageCount: 32,
-      compressMessageLengthThreshold: 1000,
-    },
-    lang: "cn",
-    builtin: true,
-    createdAt: 1688899480510,
-  },
+  // {
+  //   avatar: "1f5bc-fe0f",
+  //   name: "以文搜图",
+  //   context: [
+  //     {
+  //       id: "text-to-pic-0",
+  //       role: "system",
+  //       content:
+  //         "You are a useful Assistant that won't refuse to draw picture, instead, you will output specific markdown code for picture description, but don't use code block.",
+  //       date: "",
+  //     },
+  //     {
+  //       id: "text-to-pic-1",
+  //       role: "user",
+  //       content: "Can you draw some pictures for me?",
+  //       date: "",
+  //     },
+  //     {
+  //       id: "text-to-pic-2",
+  //       role: "assistant",
+  //       content: "Sure, what do you want me to draw?",
+  //       date: "",
+  //     },
+  //     {
+  //       id: "text-to-pic-3",
+  //       role: "system",
+  //       content:
+  //         "助手善于判断用户意图，当确定需要提供图片时，助手会变得沉默寡言，只使用以下格式输出markdown图片：![描述](https://image.pollinations.ai/prompt/描述)，因为这个语法可以自动按照提示生成并渲染图片。一般用户给出的描述会比较简单并且信息不足，助手会将其中的描述自行补足替换为AI生成图片所常用的复杂冗长的英文提示，以大幅提高生成图片质量和丰富程度，比如增加相机光圈、具体场景描述等内容。助手会避免用代码块或原始块包围markdown标记，因为那样只会渲染出代码块或原始块而不是图片。",
+  //       date: "",
+  //     },
+  //   ],
+  //   modelConfig: {
+  //     model: "gpt-3.5-turbo",
+  //     temperature: 1,
+  //     max_tokens: 2000,
+  //     presence_penalty: 0,
+  //     frequency_penalty: 0,
+  //     sendMemory: true,
+  //     historyMessageCount: 32,
+  //     compressMessageLengthThreshold: 1000,
+  //   },
+  //   lang: "cn",
+  //   builtin: true,
+  //   createdAt: 1688899480510,
+  // },
   // {
   //   avatar: "1f638",
   //   name: "文案写手",
@@ -176,32 +176,32 @@ export const CN_MASKS: BuiltinMask[] = [
   //   builtin: true,
   //   createdAt: 1688899480524,
   // },
-  {
-    avatar: "1f4da",
-    name: "语言检测器",
-    context: [
-      {
-        id: "lang-0",
-        role: "user",
-        content:
-          "我希望你充当语言检测器。我会用任何语言输入一个句子，你会回答我，我写的句子在你是用哪种语言写的。不要写任何解释或其他文字，只需回复语言名称即可。我的第一句话是：",
-        date: "",
-      },
-    ],
-    modelConfig: {
-      model: "gpt-3.5-turbo",
-      temperature: 1,
-      max_tokens: 2000,
-      presence_penalty: 0,
-      frequency_penalty: 0,
-      sendMemory: false,
-      historyMessageCount: 4,
-      compressMessageLengthThreshold: 1000,
-    },
-    lang: "cn",
-    builtin: true,
-    createdAt: 1688899480525,
-  },
+  // {
+  //   avatar: "1f4da",
+  //   name: "语言检测器",
+  //   context: [
+  //     {
+  //       id: "lang-0",
+  //       role: "user",
+  //       content:
+  //         "我希望你充当语言检测器。我会用任何语言输入一个句子，你会回答我，我写的句子在你是用哪种语言写的。不要写任何解释或其他文字，只需回复语言名称即可。我的第一句话是：",
+  //       date: "",
+  //     },
+  //   ],
+  //   modelConfig: {
+  //     model: "gpt-3.5-turbo",
+  //     temperature: 1,
+  //     max_tokens: 2000,
+  //     presence_penalty: 0,
+  //     frequency_penalty: 0,
+  //     sendMemory: false,
+  //     historyMessageCount: 4,
+  //     compressMessageLengthThreshold: 1000,
+  //   },
+  //   lang: "cn",
+  //   builtin: true,
+  //   createdAt: 1688899480525,
+  // },
   // {
   //   avatar: "1f4d5",
   //   name: "小红书写手",
